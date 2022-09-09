01ee6da4681fe57dba36e7953c7602cb9d1f5328
Jared Groves <jared.m.groves@gmail.com>
2022-09-09 14:47:29 -0700

    Added test exercise

---
+++ TestExercise.js
@@ -0,0 +1,7 @@
+// I'm another test
+let b = 15;
+console.log('b is currently ' + b)
+b++;
+console.log('b is now ' + b);
+b = b + 1
+console.log('add one more, and b is now ' + b);
--- helloworld.js
+++ helloworld.js
@@ -2,3 +2,5 @@ var a = 5;
 console.log(a + ' this is a cool test!');
 //I'm a comment! 
 //Do me next 
+console.log(a + 5);
+console.log(a+15);
